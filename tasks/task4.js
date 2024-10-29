202a19f10a4cb9a94d9ced8821afc7ff99bcc163
forsunka340 <162183120+forsunka340@users.noreply.github.com>
2024-10-29 15:24:19 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -4,8 +4,33 @@
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
 
-function deleteUser(id) {
-  // Ваш код
+async function deleteUser(id) {
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: 'DELETE',
+    });
+    
+    deleteUser(1)
+  .then(status => {
+    if (status === 200) {
+      console.log('Користувач успішно видалений');
+    } else {
+      console.error('Виникла помилка при видаленні користувача');
+    }
+  })
+  .catch(error => {
+    console.error('Помилка:', error);
+  });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.status;
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return null;
+  }
 }
 
 console.log(deleteUser(1));
