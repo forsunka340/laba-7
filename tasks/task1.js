43d43c26147fe8b8b2b507e93fb2c91aa771f99f
forsunka340 <162183120+forsunka340@users.noreply.github.com>
2024-10-29 15:18:10 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -5,9 +5,23 @@
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
 function fetchUsers() {
-  // Ваш код
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    // Створюємо новий масив, вибираючи тільки потрібні поля
+    const users = [];
+    data.forEach(user => {
+      users.push({ id: user.id, name: user.name });
+    });
+
+    return users;
+  } catch (error) {
+    console.error('Помилка при отриманні даних:', error);
+    return [];
+  }
 }
 
-console.log(fetchUsers())
+console.log(fetchUsers());
 
 module.exports = fetchUsers;
