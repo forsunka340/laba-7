7eea13017a8a26f035cd907086f2aa50d4827117
forsunka340 <162183120+forsunka340@users.noreply.github.com>
2024-10-29 15:21:07 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -4,7 +4,7 @@
 
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
-function fetchUsers() {
+async function fetchUsers() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
